d9e443ce998f37b857f1d029d0a9e5d7bf462bb5
unknown <smarine43@gmail.com>
2021-07-27 13:04:33 +1200

    added basic auth functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
+import { useSigninCheck } from 'reactfire';
+
+
 import './App.css';
 import OdrerOfBattle from './components/crusade/OrderOfBattle';
-import NavBar from './components/structural/structural';
+import Dashboard from './components/Dashboard';
+import LogInForm from './components/LoginForm';
+import NavBar from './components/structural/NavBar';
+
+
 
 function App() {
+  const { status, data: signInCheckResult } = useSigninCheck()
+
+
   return (
     <div className="App">
       <NavBar />
+      <SignIn />
       <OdrerOfBattle />
     </div>
   );
 }
 
 export default App;
+
+function SignIn() {
+  const { status, data: signInCheckResult } = useSigninCheck()
+
+  //if (status === 'loading') {
+  //  return <h1>add loading spinner</h1>
+  //}
+  
+  if(signInCheckResult.signedIn === true) {
+    return <Dashboard />
+  } else {
+    return <LogInForm />
+  }
+}
--- src/components/structural/structural.tsx
+++
@@ -1,9 +0,0 @@
-export default function NavBar() {
-  
-  
-  return (
-    <nav className="top-nav">
-      <h1>React Test App</h1>
-    </nav>
-  )
-}
